02d1f9396cdf5769f1e63e939691d139b341070d
Rodrigo Fines <112434197+Rodrigooo08@users.noreply.github.com>
2024-11-02 12:34:15 -0300

    Agrego carpeta sound con los archivos de sonido para juegonave

--- proyectofinal_grupo_n3/src/Components/JuegoNave/Juego.jsx
+++ proyectofinal_grupo_n3/src/Components/JuegoNave/Juego.jsx
@@ -4,32 +4,32 @@ import Escena2 from "./scenes/Escena2.js"
 import EscenaBonus from "./scenes/EscenaBonus.js"
 import GameOver from "./scenes/GameOver.js";
 import Victoria from "./scenes/Victoria.js";
-import MenuStart from "./scenes/MenuStart.js"; 
+import MenuStart from "./scenes/MenuStart.js";
 import Escena3 from "./scenes/Escena3.js";
 import Escena4 from "./scenes/Escena4.js";
 import Ajustes from "./scenes/Ajustes.js";
 import { useEffect } from 'react';
 
-function Juego(){
-    useEffect(()=>{
-        const config={
+function Juego() {
+    useEffect(() => {
+        const config = {
             type: Phaser.AUTO,
             width: 800,
             height: 600,
-            parent:'game-container',
-            scene: [MenuStart,Escena1, Escena2, EscenaBonus, Escena3, Escena4, GameOver, Ajustes, Victoria],
-            physics:{
+            parent: 'game-container',
+            scene: [MenuStart, Escena1, Escena2, EscenaBonus, Escena3, Escena4, GameOver, Ajustes, Victoria],
+            physics: {
                 default: 'arcade',
-                arcade:{
-                    gravity:{y:0},
+                arcade: {
+                    gravity: { y: 0 },
                     debug: false
                 }
             },
         }
         let game = new Phaser.Game(config);
     });
-    return ( 
-        <div id = "Conteiner">
+    return (
+        <div id="Conteiner">
             <h1 id="Titulo">Esquivando Meteoros🚀</h1>
             <div id="game-container"></div>
         </div>

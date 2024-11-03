1c3f65b200a5dca28699bce8648304ab9a1528cc
LIFabrego <165028180+LIFabrego@users.noreply.github.com>
2024-11-03 12:32:01 -0300

    actualizacion case Intermedio
    se agrega el codigo de intermedio para generar operaciones de division y multiplicacion

--- proyectofinal_grupo_n3/src/Components/DesafioMatematico/GenerarOperaciones.jsx
+++ proyectofinal_grupo_n3/src/Components/DesafioMatematico/GenerarOperaciones.jsx
@@ -12,6 +12,13 @@ function generarOperacion(dificultad) {
         : { pregunta: `${Math.max(num1, num2)} - ${Math.min(num1, num2)}`, respuesta: Math.abs(num1 - num2) };
 
     case 'intermedio':
+      tipo = Math.random() > 0.5 ? 'multiplicacion' : 'division';
+      num1 = Math.floor(Math.random() * 10) + 1;
+      num2 = Math.floor(Math.random() * 10) + 1;
+      return tipo === 'multiplicacion'
+        ? { pregunta: `${num1} × ${num2}`, respuesta: num1 * num2 }
+        : { pregunta: `${num1 * num2} ÷ ${num2}`, respuesta: num1 };
+
 
     case 'avanzado':
 

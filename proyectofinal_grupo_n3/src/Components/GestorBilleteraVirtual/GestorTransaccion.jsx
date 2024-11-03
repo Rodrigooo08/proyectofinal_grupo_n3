fb99b09f1ba0b23c80ea33967687904ce815bfad
Solefloo <147277045+Solefloo@users.noreply.github.com>
2024-11-03 15:10:07 -0300

    cambiio y correciones

--- proyectofinal_grupo_n3/src/Components/GestorBilleteraVirtual/GestorTransaccion.jsx
+++ proyectofinal_grupo_n3/src/Components/GestorBilleteraVirtual/GestorTransaccion.jsx
@@ -11,6 +11,7 @@ function GestorTransacciones() {
   const [listaMaximaTransaccionesVisible, setListaMaximaTransaccionesVisible] = useState(false);
   const [transaccionMaxima, setTransaccionMaxima] = useState('');
 
+
   const agregarUsuarios = () => {
     if (!nombre || !billetera || isNaN(parseInt(transaccion, 10))) {
       alert('Por favor, completa todos los campos correctamente.');
@@ -24,42 +25,70 @@ function GestorTransacciones() {
     setTransaccion('');
     // cargaAlerta();
     ocultarListas();
+    listaUsuarios();
 
   };
 
+  function listaUsuarios() {
 
+    const contenedorLista = document.getElementById('contenedorLista');
+    contenedorLista.innerHTML = '';
 
-  const billeteraMasTransacciones = () => {
+    //Muestra de registro de Usuario fue cambiado, de div a li para que se muestre en pantalla en forma de lista
+    usuarios.forEach(cuenta => {
+      const li = document.createElement('li');
+      li.classList.add('user-list-item');
+      li.textContent = `${cuenta.nombre}, ${cuenta.billetera}, ${cuenta.transaccion}`;
+      contenedorLista.appendChild(li);
+    });
+
+  }
+
+  function billeteraMasTransacciones() {
+    const transaccionMaxima = document.getElementById('transaccionMaxima');
     if (listaMaximaTransaccionesVisible) {
-      setTransaccionMaxima('');
+      transaccionMaxima.innerHTML = '';
       setListaMaximaTransaccionesVisible(false);
       return;
     }
-
     if (usuarios.length === 0) return;
-
     const transaccionesPorUsuario = {};
-    usuarios.forEach(({ nombre, billetera, transaccion }) => {
-      if (!transaccionesPorUsuario[nombre]) {
-        transaccionesPorUsuario[nombre] = {};
+    usuarios.forEach(function (transaccion) {
+      if (!transaccionesPorUsuario[transaccion.nombre]) {
+        transaccionesPorUsuario[transaccion.nombre] = {};
       }
-      transaccionesPorUsuario[nombre][billetera] = (transaccionesPorUsuario[nombre][billetera] || 0) + transaccion;
+      //en caso de que las billeteras esten repetidas para un mismo usuario se suma el numero de transacciones
+      if (transaccionesPorUsuario[transaccion.nombre][transaccion.billetera]) {
+        transaccionesPorUsuario[transaccion.nombre][transaccion.billetera] += transaccion.transaccion;
+      } else {
+        transaccionesPorUsuario[transaccion.nombre][transaccion.billetera] = transaccion.transaccion;
+      }
     });
-
+    //muestra del resultado maximo por cada uno de los usuarios
     let resultado = '';
     Object.entries(transaccionesPorUsuario).forEach(([nombre, billetera]) => {
       const billeteraConMasTransacciones = Object.entries(billetera).reduce((prev, current) => (prev[1] > current[1] ? prev : current));
       resultado += `${nombre}: ${billeteraConMasTransacciones[0]} ${billeteraConMasTransacciones[1]} transacciones <br>`;
+      console.log(resultado);
     });
 
+    transaccionMaxima.innerHTML = resultado;
     setTransaccionMaxima(resultado);
     setListaMaximaTransaccionesVisible(true);
   }
 
-  const ocultarListas = () => {
-    setTransaccionMaxima('');
+  function ocultarListas() {
+
+    //oculta lista de billetera con mas Transacciones
+    transaccionMaxima.innerHTML = '';
     setListaMaximaTransaccionesVisible(false);
-  };
+  }
+  //   function cargaAlerta(){
+  //     swal({
+  //         text: "Se ha cargado correctamente",
+  //         icon: "success",
+  //     });
+  // }
   return (
     <>
       <div className='Formulario'>
@@ -75,6 +104,7 @@ function GestorTransacciones() {
           billeteraMasTransacciones={billeteraMasTransacciones}
           transaccionMaxima={transaccionMaxima}
           GestorTransacciones={GestorTransacciones}
+          listaMaximaTransaccionesVisible={listaMaximaTransaccionesVisible}
         />
       </div>
 

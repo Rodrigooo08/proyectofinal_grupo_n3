91b59363d5af496aaa6e4a31244bd3c2bc6f68bf
CintiaFlores <108830824+CintiaFlores@users.noreply.github.com>
2024-11-08 19:38:16 -0300

    FloresNoemi-Modificacion en AboutUs
    cambie el estilo de las tarjetas de colaboradores

--- proyectofinal_grupo_n3/src/Components/HomePage/AboutUs.jsx
+++ proyectofinal_grupo_n3/src/Components/HomePage/AboutUs.jsx
@@ -1,31 +1,42 @@
-function AboutUs({ lista }) {
-    const colaboradores = lista.map((colaborador) => (
-    <div className="col" key={colaborador.id}>
-        <div className="card w-50 mb-3 d-inline-flex p-2">
-            <img src={colaborador.image} className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{colaborador.name}</h5>
-                <p className="card-text">{colaborador.descripcion}</p> </div>
-        </div> 
-        </div>));
-    return (
-        <> <section className="section1">
-           
-            <nav className="navbar bg-body-tertiary">
-                <div className="container-fluid">
-                    <span className="navbar-brand mb-0 h1">Equipo de desarrollo del Grupo 3</span>
+import Button from 'react-bootstrap/Button'; 
+function AboutUs({ lista }) {  
+  const colaboradores = lista.map((colaborador) => (  
+    <div className="card text-bg-dark border-light mb-3" key={colaborador.id}>  
+      <div className="row g-0">  
+        <div className="col-md-4">  
+          <img src={colaborador.image} className="img-fluid rounded-start" />  
+        </div>  
+        <div className="col-md-8">  
+          <div className="card-body">  
+            <h5 className="card-title">{colaborador.name}</h5>  
+            <p className="card-text text-start">{colaborador.descripcion}</p>  
+            <Button   
+              variant="dark"   
+              onClick={() => window.location.href = colaborador.url}  
+            >  
+              Perfil GitHub  
+            </Button>  
+          </div>  
+        </div>  
+      </div>  
+    </div>  
+  ));  
 
-                </div>
-            </nav>
-        </section>
-            <section id="section2" className="section2 container-fluid">
-                <div className="row row-cols-1 row-cols-md-2 mx-auto p-2">
-                    {colaboradores}
-                </div>
-            </section>
-        </>
-
-
-    );
-}
+  return (  
+    <>  
+      <section className="section1">  
+        <nav className="navbar bg-body-tertiary">  
+          <div className="container-fluid">  
+            <span className="navbar-brand mb-0 h1">Equipo de desarrollo del Grupo 3</span>  
+          </div>  
+        </nav>  
+      </section>  
+      <section id="section2" className="section2 container-fluid">  
+        <div>  
+          {colaboradores}  
+        </div>  
+      </section>  
+    </>  
+  );  
+}  
 export default AboutUs;

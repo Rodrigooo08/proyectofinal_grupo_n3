d8ef13da68b548f585c5064b0cc8a900e3b7c19f
Ralseik <165042055+Ralseik@users.noreply.github.com>
2024-11-07 22:13:21 -0300

    cambios DesafioM
    cambios en el css y agregar imagen de dialogo

---
+++ proyectofinal_grupo_n3/src/Components/DesafioMatematico/DialogoVegeta.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DialogoVegeta = () => {
+  return (
+<div className='Dialogo'>
+<img src="../public/image/DesafioMatematicoIMG/DialogoVegeta.png" alt="Dialogo para la seleccion de Dificultad" />  
+<h2>Elige una dificultad</h2>
+</div>
+
+ );
+};
+
+export default DialogoVegeta;

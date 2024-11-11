76750953c282b554ba9dd5ed3472bd81f8456a35
LIFabrego <165028180+LIFabrego@users.noreply.github.com>
2024-11-11 19:40:28 -0300

    limitacion para Altura
    se agrega una limitacion a la altura para que se pueden ingresar valores de 0.5 a 2.5m

--- proyectofinal_grupo_n3/src/Components/CalculadoraIMC/Formulario.jsx
+++ proyectofinal_grupo_n3/src/Components/CalculadoraIMC/Formulario.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 function Formulario({ nombre, apellido, peso, altura, setNombre, setApellido, setPeso, setAltura, calcular }) {
-  const validacionTexto = (settext) => (e) => {
+  const limitacionTexto = (settext) => (e) => {
     const value = e.target.value;
     if (/^[A-Za-z\s]*$/.test(value) || value === "") { 
       settext(value);
     }
   };
-  const validacionPeso= (e) =>{
+  const limitacionPeso= (e) =>{
     let value = e.target.value;
     if (value === "") {
       setPeso(value);
@@ -16,19 +16,28 @@ function Formulario({ nombre, apellido, peso, altura, setNombre, setApellido, se
       if (value >= 1 && value <= 300) setPeso(value);
     }
   };
+  const limitacionAltura=(e) =>{
+    let value = e.target.value;
+    if (value === ""){
+      setPeso(value);
+    } else {
+      value = Math.min(parseFloat(value), 2.5);
+      if (value >= 0.5 && value <= 2.5) setAltura(value);
+    }
+  }
   return (
     <div>
       <label className='letras'>Nombre:</label>
-      <input className='entradaIMC' type="text" value={nombre} onChange={validacionTexto(setNombre)} />
+      <input className='entradaIMC' type="text" value={nombre} onChange={limitacionTexto(setNombre)} />
 
         <label className='letras'>Apellido:</label>
-      <input className='entradaIMC' type="text" value={apellido} onChange={validacionTexto(setApellido)}/>
+      <input className='entradaIMC' type="text" value={apellido} onChange={limitacionTexto(setApellido)}/>
 
        <label className='letras'>Peso (Kg):</label>
-      <input className='entradaIMC' type="number" value={peso} onChange={validacionPeso} min="1" max="300" title="Ingrese un peso entre 1 y 300 kg"/>
+      <input className='entradaIMC' type="number" value={peso} onChange={limitacionPeso} min="1" max="300" title="Ingrese un peso entre 1 y 300 kg"/>
 
        <label className='letras'>Altura (m):</label>
-      <input className='entradaIMC' type="number" value={altura} onChange={(e) => setAltura(e.target.value)}/>
+      <input className='entradaIMC' type="number" value={altura} onChange={limitacionAltura} min="0.5" max="2.5" step="0.01" />
 
       <button className="boton" onClick={calcular}>Calcular IMC</button>
     </div>

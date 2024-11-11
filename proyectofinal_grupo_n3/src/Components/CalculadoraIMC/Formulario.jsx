30b4167cee14d0ff7693a9cbbf89d48f9e83af99
LIFabrego <165028180+LIFabrego@users.noreply.github.com>
2024-11-11 18:29:39 -0300

    ajuste Formulario IMC
    se agrega una validacion de texto que permita solo el ingreso de caracteres alfabeticos y espacios vacios para los campos de Nombre y Apellido

--- proyectofinal_grupo_n3/src/Components/CalculadoraIMC/Formulario.jsx
+++ proyectofinal_grupo_n3/src/Components/CalculadoraIMC/Formulario.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
 function Formulario({ nombre, apellido, peso, altura, setNombre, setApellido, setPeso, setAltura, calcular }) {
+  const validacionTexto = (settext) => (e) => {
+    const value = e.target.value;
+    if (/^[A-Za-z\s]*$/.test(value) || value === "") { // Solo permite letras y espacios
+      settext(value);
+    }
+  };
   return (
     <div>
       <label className='letras'>Nombre:</label>
-      <input className='entradaIMC' type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+      <input className='entradaIMC' type="text" value={nombre} onChange={validacionTexto(setNombre)} />
 
         <label className='letras'>Apellido:</label>
-      <input className='entradaIMC' type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+      <input className='entradaIMC' type="text" value={apellido} onChange={validacionTexto(setApellido)}/>
 
        <label className='letras'>Peso (Kg):</label>
-      <input className='entradaIMC' type="number" value={peso} onChange={(e) => setPeso(e.target.value)} />
+      <input className='entradaIMC' type="number" value={peso} onChange={(e) => setPeso(e.target.value)}/>
 
        <label className='letras'>Altura (m):</label>
-      <input className='entradaIMC' type="number" value={altura} onChange={(e) => setAltura(e.target.value)} />
+      <input className='entradaIMC' type="number" value={altura} onChange={(e) => setAltura(e.target.value)}/>
 
       <button className="boton" onClick={calcular}>Calcular IMC</button>
     </div>

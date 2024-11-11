0569a20247cd7defffa900346bd4622e5bdbdaf9
CintiaFlores <108830824+CintiaFlores@users.noreply.github.com>
2024-11-11 12:43:56 -0300

    Modificacion en Home
    agregue imagenes, coloque links,agregue en homestyle

--- proyectofinal_grupo_n3/src/Components/HomePage/Home.jsx
+++ proyectofinal_grupo_n3/src/Components/HomePage/Home.jsx
@@ -46,29 +46,59 @@ function Home() {
                             <div className="card" >
                                 <img src="Image/HomePage/construccion1.jpg" className="card-img-top" alt="..." />
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
+                                    <h5 className="card-title">Inicio</h5>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link to="#" className="btn btn-primary">Go somewhere</Link>
+                                    <Link to="http://localhost:5173/" className="btn btn-primary">Go somewhere</Link>
                                 </div>
                             </div>
                         </div>
                         <div className="col-12 col-md-4">
                             <div className="card">
-                                <img src="Image/HomePage/construccion3.jpg" className="card-img-top" alt="..." />
+                                <img src="Image/HomePage/capIMC.png" className="card-img-top" alt="..." />
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
+                                    <h5 className="card-title">Calculador IMC</h5>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link to="#" className="btn btn-primary">Go somewhere</Link>
+                                    <Link to="http://localhost:5173/calculadoraIMC" className="btn btn-primary">Go somewhere</Link>
                                 </div>
                             </div>
                         </div>
                         <div className="col-12 col-md-4">
                             <div className="card" >
-                                <img src="Image/HomePage/construccion4.jpg" className="card-img-top" alt="..." />
+                                <img src="Image/HomePage/capGB.png" className="card-img-top" alt="..." />
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
+                                    <h5 className="card-title">Gestor de Billeteras</h5>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link to="#" className="btn btn-primary">Go somewhere</Link>
+                                    <Link to="http://localhost:5173/gestorbilleteras" className="btn btn-primary">Go somewhere</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-4">
+                            <div className="card" >
+                                <img src="Image/HomePage/capNave.png" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Juego Nave</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <Link to="http://localhost:5173/juegonave" className="btn btn-primary">Go somewhere</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-4">
+                            <div className="card" >
+                                <img src="Image/HomePage/capDM..png" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Desafio Matematico</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <Link to="http://localhost:5173/desafiomatematico" className="btn btn-primary">Go somewhere</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-4">
+                            <div className="card" >
+                                <img src="Image/HomePage/capColab.png" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Colaboradores</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <Link to="http://localhost:5173/nosotros" className="btn btn-primary">Go somewhere</Link>
                                 </div>
                             </div>
                         </div>

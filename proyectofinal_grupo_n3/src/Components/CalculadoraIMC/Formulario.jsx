35a7e2d6bfd9577d88f1a00123325d03fd199627
LIFabrego <165028180+LIFabrego@users.noreply.github.com>
2024-11-11 19:09:56 -0300

    liimitacion en ingreso Peso
    se liumita el ingreso de Peso en el rango de 1 a 300 kg

--- proyectofinal_grupo_n3/src/Components/CalculadoraIMC/Formulario.jsx
+++ proyectofinal_grupo_n3/src/Components/CalculadoraIMC/Formulario.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 function Formulario({ nombre, apellido, peso, altura, setNombre, setApellido, setPeso, setAltura, calcular }) {
   const validacionTexto = (settext) => (e) => {
     const value = e.target.value;
-    if (/^[A-Za-z\s]*$/.test(value) || value === "") { // Solo permite letras y espacios
+    if (/^[A-Za-z\s]*$/.test(value) || value === "") { 
       settext(value);
+    }
+  };
+  const validacionPeso= (e) =>{
+    let value = e.target.value;
+    if (value === "") {
+      setPeso(value);
+    } else {
+      value = Math.min(parseFloat(value), 300); 
+      if (value >= 1 && value <= 300) setPeso(value);
     }
   };
   return (
@@ -16,7 +25,7 @@ function Formulario({ nombre, apellido, peso, altura, setNombre, setApellido, se
       <input className='entradaIMC' type="text" value={apellido} onChange={validacionTexto(setApellido)}/>
 
        <label className='letras'>Peso (Kg):</label>
-      <input className='entradaIMC' type="number" value={peso} onChange={(e) => setPeso(e.target.value)}/>
+      <input className='entradaIMC' type="number" value={peso} onChange={validacionPeso} min="1" max="300" title="Ingrese un peso entre 1 y 300 kg"/>
 
        <label className='letras'>Altura (m):</label>
       <input className='entradaIMC' type="number" value={altura} onChange={(e) => setAltura(e.target.value)}/>
